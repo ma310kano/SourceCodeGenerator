5747bd2653a0dd12f8cf3e17b083c226ce7b21f4
ma310kano <ma310kano@gmail.com>
2025-02-05 22:30:24 +0900

    レコード型へ変更

--- SourceCodeGeneration/Application/Command/ValueObjectMaximumLengthGenerationCommand.cs
+++ SourceCodeGeneration/Application/Command/ValueObjectMaximumLengthGenerationCommand.cs
@@ -3,63 +3,8 @@
 /// <summary>
 /// 値オブジェクトの生成コマンド
 /// </summary>
-public class ValueObjectMaximumLengthGenerationCommand
-{
-    #region Constructors
-
-    /// <summary>
-    /// 値オブジェクトの生成コマンドを初期化します。
-    /// </summary>
-    /// <param name="className">クラス名</param>
-    /// <param name="classDescription">クラスの説明</param>
-    /// <param name="namespaceSequence">名前空間シーケンス</param>
-    /// <param name="maximumLength">最大長</param>
-    public ValueObjectMaximumLengthGenerationCommand(string className, string classDescription, string namespaceSequence, int maximumLength)
-    {
-        ClassName = className;
-        ClassDescription = classDescription;
-        NamespaceSequence = namespaceSequence;
-        MaximumLength = maximumLength;
-    }
-
-    #endregion
-
-    #region Properties
-
-    /// <summary>
-    /// クラス名を取得します。
-    /// </summary>
-    public string ClassName { get; }
-
-    /// <summary>
-    /// クラスの説明を取得します。
-    /// </summary>
-    public string ClassDescription { get; }
-
-    /// <summary>
-    /// 名前空間シーケンスを取得します。
-    /// </summary>
-    public string NamespaceSequence { get; }
-
-    /// <summary>
-    /// 最大長を取得します。
-    /// </summary>
-    public int MaximumLength { get; }
-
-    #endregion
-
-    #region Methods
-
-    /// <summary>
-    /// 現在のオブジェクトを表す文字列を返します。
-    /// </summary>
-    /// <returns>現在のオブジェクトを表す文字列。</returns>
-    public override string ToString()
-    {
-        string str = $"{nameof(ValueObjectMaximumLengthGenerationCommand)} {{ {nameof(ClassName)} = {ClassName}, {nameof(ClassDescription)} = {ClassDescription}, {nameof(NamespaceSequence)} = {NamespaceSequence}, {nameof(MaximumLength)} = {MaximumLength} }}";
-
-        return str;
-    }
-
-    #endregion
-}
+/// <param name="ClassName">クラス名</param>
+/// <param name="ClassDescription">クラスの説明</param>
+/// <param name="NamespaceSequence">名前空間シーケンス</param>
+/// <param name="MaximumLength">最大長</param>
+public record class ValueObjectMaximumLengthGenerationCommand(string ClassName, string ClassDescription, string NamespaceSequence, int MaximumLength);
